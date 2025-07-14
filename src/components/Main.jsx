fef9600319ae70221984d201ed7e99f34f5f523f
Fernando Warno <ferwar@hotmail.com>
2025-07-14 03:21:54 -0300

    Se eliminan archivos

--- src/components/Main.jsx
+++
@@ -1,9 +0,0 @@
-function Main() {
-  return (
-    <main className="container my-5 text-center">
-      <h1 style={{ color: 'var(--color-text-primary)' }}>Contenido Principal</h1>
-      <p className="lead" style={{ color: 'var(--color-text-muted)' }}>Este es un ejemplo de contenido dentro del área principal</p>
-    </main>
-  );
-}
-export default Main;
